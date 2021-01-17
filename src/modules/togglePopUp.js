8bb868fb54a6a7634a7118fa2622c69d84c1b83a
akademik2607 <akademik2607@mail.ru>
2021-01-17 07:50:50 +0300

    Добавил закрытие модального окна

--- src/modules/togglePopUp.js
+++ src/modules/togglePopUp.js
@@ -1,8 +1,8 @@
 const togglePopUp = () => {
     const callbackBtn = document.querySelectorAll('.callback-btn'),    
         modalCallback = document.querySelector('.modal-callback'), 
-        modalOverlay = document.querySelector('.modal-overlay');
-
+        modalOverlay = document.querySelector('.modal-overlay'),
+        modalClose = document.querySelector('.modal-close');
     callbackBtn.forEach( (item) => {
         item.addEventListener('click', (event) => {
             event.preventDefault();
@@ -10,6 +10,13 @@ const togglePopUp = () => {
             modalCallback.style.display = 'block';
         });
 
+    const closePopUp = () => {
+        modalOverlay.style.display = 'none';
+        modalCallback.style.display = 'none';
+    };
+
+    modalClose.addEventListener('click', closePopUp);
+    modalOverlay.addEventListener('click', closePopUp);
     console.log(callbackBtn);
     });
 };
