9d4dff830e9fdd3f3d28241bf547ceb6c292a55c
akademik2607 <akademik2607@mail.ru>
2021-01-20 00:59:45 +0300

    Добавил открытие модального окна по кнопке .button-services

--- src/modules/togglePopUp.js
+++ src/modules/togglePopUp.js
@@ -1,23 +1,29 @@
 const togglePopUp = () => {
     const callbackBtn = document.querySelectorAll('.callback-btn'),    
         modalCallback = document.querySelector('.modal-callback'), 
+        buttonServices = document.querySelector('.button-services'),
         modalOverlay = document.querySelector('.modal-overlay'),
         modalClose = document.querySelector('.modal-close');
+
+    const openPopUp = (event) => {
+        event.preventDefault();
+        modalOverlay.style.display = 'block';
+        modalCallback.style.display = 'block';
+    };
+
     callbackBtn.forEach( (item) => {
-        item.addEventListener('click', (event) => {
-            event.preventDefault();
-            modalOverlay.style.display = 'block';
-            modalCallback.style.display = 'block';
-        });
-
-        const closePopUp = () => {
-            modalOverlay.style.display = 'none';
-            modalCallback.style.display = 'none';
-        };
-
-        modalClose.addEventListener('click', closePopUp);
-        modalOverlay.addEventListener('click', closePopUp);
+        item.addEventListener('click', openPopUp);
     });
+
+    buttonServices.addEventListener('click', openPopUp);
+
+    const closePopUp = () => {
+        modalOverlay.style.display = 'none';
+        modalCallback.style.display = 'none';
+    };
+
+    modalClose.addEventListener('click', closePopUp);
+    modalOverlay.addEventListener('click', closePopUp);
 };
 
 export default togglePopUp;
